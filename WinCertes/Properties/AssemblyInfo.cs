9933442b6f9a00dbcf2d7e30ad6aa68ac7bd5974
Alexandre Aufrere <loopkin@nikosoft.net>
2020-02-17 15:16:56 +0100

    Upgrading WinCertes version number

--- WinCertes/Properties/AssemblyInfo.cs
+++ WinCertes/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinCertes")]
-[assembly: AssemblyCopyright("Copyright ©  2018-2019 Alexandre Aufrere")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2020 Alexandre Aufrere")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
